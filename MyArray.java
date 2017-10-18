e0e803bbf2265515b26790fb19f6260589be9085
minhnln <mingngo158@gmail.com>
2017-10-19 01:07:55 +1000

    create getElements() and equals() methods

--- MyArray.java
+++ MyArray.java
@@ -14,9 +14,39 @@ public class MyArray {
 	}
 	
 	/*
+	 * Constructs a new Array instance from an array
+	 * @param array: is used to clone
+	 */
+	public MyArray(int[] array) {
+		elements = array.clone();
+	}
+	
+	/*
 	 * Accessor methods
 	 */
 	public int getSize() {
 		return elements.length;
+	}
+	
+	public int[] getElements () {
+		return elements;
+	}
+	
+	/*
+	 * Equal method
+	 * @param array   : the array is compared with the elements of MyArray instance
+	 * @return boolean: true if all the element of array and MyArray instance are equal
+	 */
+	public boolean equals (int[] array) {
+		if (this.getSize() != array.length) {
+			return false;
+		}
+		
+		for (int i = 0; i < this.getSize(); i++) {
+			if (this.elements[i] != array[i])
+				return false;
+		}
+		
+		return true;
 	}
 }
