31d6f3a868bb3b0938ce3c9133ccf27a14ec9ac3
minhnln <mingngo158@gmail.com>
2017-10-19 01:12:39 +1000

    update MyArrayTest.java: add test for contructor by array param

--- MyArrayTest.java
+++ MyArrayTest.java
@@ -3,11 +3,18 @@ import static org.junit.Assert.*;
 import org.junit.Test;
 
 public class MyArrayTest {
-	final static int SIZE_ARRAY = 10;
+	final static int ARRAY_SIZE = 10;
+	final static int[] ARRAY_TEST = {1, 2, 3, 4, 5};
 	@Test
 	public void testConstructor() {
-		MyArray myArray = new MyArray(SIZE_ARRAY);
-		assertEquals(SIZE_ARRAY, myArray.getSize());
+		MyArray myArray_1 = new MyArray(ARRAY_SIZE);
+		assertEquals(ARRAY_SIZE, myArray_1.getSize());
+		
+		MyArray myArray_2 = new MyArray(ARRAY_TEST);
+		if (!myArray_2.equals(ARRAY_TEST)) {
+			fail();
+		}
+//		assertEquals(ARRAY_TEST, myArray_2);
 	}
 
 }
