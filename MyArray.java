a556bc1dded89e1e147b23da16356d5a18381a54
minhnln <mingngo158@gmail.com>
2017-10-19 00:24:17 +1000

    Create constructor by size param

---
+++ MyArray.java
@@ -0,0 +1,15 @@
+/*
+ * Simple version of Array
+ */
+
+public class MyArray {
+	int[] elements;
+	
+	/*
+	 * Constructor a new Array instance
+	 * @param size: the size of the Array instance
+	 */
+	public MyArray(int size) {
+		elements = new int[size];
+	}
+}
