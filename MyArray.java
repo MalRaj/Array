e7d0b8f1743c9a8e3df5134b78a9fd9835ca63ac
minhnln <mingngo158@gmail.com>
2017-10-19 21:29:30 +1000

    add constructor with start, stop and step param, add test for this constructor

--- MyArray.java
+++ MyArray.java
@@ -33,6 +33,40 @@ public class MyArray {
 	}
 	
 	/*
+	 * Constructs a new Array instance in range from start(not included stop point)
+	 *  to stop by step
+	 * @param start: the start of range value for the Array instance
+	 * @param stop:  the end of range value for the Array instance
+	 * @param step:  the difference between 2 consecutive elements of the Array
+	 * @return    :  null if step = 0 or start > stop
+	 */
+	public MyArray(int start, int stop, int step) {
+		int range = stop - start;
+		int size = 0;
+		try {
+			size = (range - 1) / step + 1;
+		}
+		catch (ArithmeticException e) {
+			System.out.println("You should not divide a number by zero");
+		}
+		
+		if (0 >= size) {
+			elements = null; 
+		}
+		else {
+			elements = new int[size];
+			
+			// Generate a range number from start to stop by the step (not include stop)
+			elements[0] = start;
+			if (size > 1) {
+				for (int i = 1; i < size; i++) {
+					elements[i] = elements[i-1] + step;
+				}
+			}
+		}
+	}
+	
+	/*
 	 * Equal method
 	 * @param array   : the array is compared with the elements of MyArray instance
 	 * @return boolean: true if all the element of array and MyArray instance are equal
--- MyArrayTest.java
+++ MyArrayTest.java
@@ -1,20 +1,48 @@
 import static org.junit.Assert.*;
 
+import java.util.Arrays;
+
 import org.junit.Test;
 
-public class MyArrayTest {
-	final static int ARRAY_SIZE = 10;
-	final static int[] ARRAY_TEST = {1, 2, 3, 4, 5};
+public class MyArrayTest {	
 	@Test
 	public void testConstructor() {
-		MyArray myArray_1 = new MyArray(ARRAY_SIZE);
-		assertEquals(ARRAY_SIZE, myArray_1.getSize());
+		int[] expectedArray_1 = {0, 0, 0, 0, 0};
+		MyArray myArray_1 = new MyArray(5);
+		assertEquals("Failed: Created instance by size", 
+				Arrays.toString(expectedArray_1), 
+				Arrays.toString(myArray_1.getElements()));
 		
-		MyArray myArray_2 = new MyArray(ARRAY_TEST);
-		if (!myArray_2.equals(ARRAY_TEST)) {
-			fail();
-		}
-//		assertEquals(ARRAY_TEST, myArray_2);
+		int[] expectedArray_2 = {1, 2, 3, 4, 5};
+		MyArray myArray_2 = new MyArray(expectedArray_2);
+		assertEquals("Failed: Created instance by an array",
+				Arrays.toString(expectedArray_2), 
+				Arrays.toString(myArray_2.getElements()));
+		
+		
+		MyArray myArray_3 = new MyArray(0, 10, 3);
+		int[] expectedArray_3 = {0, 3, 6, 9};
+		assertEquals("Failed: Created instance by start, stop, and step",
+				Arrays.toString(expectedArray_3), 
+				Arrays.toString(myArray_3.getElements()));
+		
+		MyArray myArray_4 = new MyArray(0, 10, 0);
+		int[] expectedArray_4 = null;
+		assertEquals("Failed: Created instance by start, stop, and step with step = 0",
+				Arrays.toString(expectedArray_4), 
+				Arrays.toString(myArray_4.getElements()));
+		
+		MyArray myArray_5 = new MyArray(10, 10, 2);
+		int[] expectedArray_5 = {10};
+		assertEquals("Failed: Created instance by start, stop, and step with start = stop",
+				Arrays.toString(expectedArray_5), 
+				Arrays.toString(myArray_5.getElements()));
+		
+		MyArray myArray_6 = new MyArray(10, 0, 3);
+		int[] expectedArray_6 = null;
+		assertEquals("Failed: Created instance by start, stop, and step with start > stop",
+				Arrays.toString(expectedArray_6), 
+				Arrays.toString(myArray_6.getElements()));
 	}
 
 }
